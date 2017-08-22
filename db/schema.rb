5fdd2f6917757abd86230137faa648694cc87acd
Frida Kiriakos <f.kiriakos@boundlessdrop.com>
2017-08-22 10:43:21 +0300

    using postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170821133005) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "jobs", force: :cascade do |t|
     t.string "position"
     t.text "description"
